7c4054c0c2b3e358e1da483de9a00c788d5d6631
Achmad Farizi <farizi1412@gmail.com>
2025-05-23 11:21:58 +0700

    first init

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import path from "path";
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
-  publicDir: resolve(__dirname, "src", "public"), // Ensure this is correct
+  publicDir: resolve(__dirname, "src", "public"), // Ensure assets like images and manifest are in the public folder
   build: {
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src", "index.html"),
+        main: resolve(__dirname, "src", "index.html"), // Make sure Vite uses the correct entry HTML file
       },
     },
   },
@@ -20,7 +20,7 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  base: "/Distory-Dicoding/", // Set this to your GitHub repository name
+  base: "/Distory-Dicoding/", // Set base to GitHub repo name for deployment
   plugins: [
     {
       name: "copy-service-worker",
